13b915c73e1b2de93aed109977c38693cada45a3
neko_the_shadow <h.nakamura0903@gmail.com>
2023-05-04 23:19:50 +0900

    9.2 データベース登録プログラム

---
+++ cmd/aozora-collector/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
+	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/text/encoding/japanese"
+)
+
+type Entry struct {
+	AuthorID string
+	Author   string
+	TitleID  string
+	Title    string
+	InfoURL  string
+	ZipURL   string
+}
+
+func findEntries(siteURL string) ([]Entry, error) {
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return nil, err
+	}
+
+	pat := regexp.MustCompile(`.*/cards/([0-9]+)/card([0-9]+).html$`)
+	entries := []Entry{}
+	doc.Find("ol li a").Each(func(n int, elem *goquery.Selection) {
+		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+		if len(token) != 3 {
+			return
+		}
+
+		title := elem.Text()
+		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+		author, zipURL := findAuthorAndZIP(pageURL)
+		if zipURL != "" {
+			entries = append(entries, Entry{
+				AuthorID: token[1],
+				Author:   author,
+				TitleID:  token[2],
+				Title:    title,
+				InfoURL:  siteURL,
+				ZipURL:   zipURL,
+			})
+		}
+	})
+
+	return entries, nil
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	doc, err := goquery.NewDocument(siteURL)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").First().Text()
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+
+	if zipURL == "" {
+		return author, ""
+	}
+	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+		return author, zipURL
+	}
+
+	u, err := url.Parse(siteURL)
+	if err != nil {
+		return author, ""
+	}
+	u.Path = path.Join(path.Dir(u.Path), zipURL)
+
+	return author, u.String()
+}
+
+func main() {
+	db, err := setupDB("database.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+	entries, err := findEntries(listURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, entry := range entries {
+		content, err := extractText(entry.ZipURL)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		err = addEntry(db, &entry, content)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+}
+
+func extractText(zipURL string) (string, error) {
+	resp, err := http.Get(zipURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+	if err != nil {
+		return "", err
+	}
+
+	for _, file := range r.File {
+		if path.Ext(file.Name) == ".txt" {
+			f, err := file.Open()
+			if err != nil {
+				return "", err
+			}
+			defer f.Close()
+
+			b, err := ioutil.ReadAll(f)
+			if err != nil {
+				return "", err
+			}
+
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+			if err != nil {
+				return "", err
+			}
+			return string(b), nil
+		}
+	}
+
+	return "", fmt.Errorf("contents not found: %v", zipURL)
+}
+
+func setupDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS authors (
+			author_id TEXT,
+			author    TEXT,
+			PRIMARY KEY (author_id)
+		)`,
+		`CREATE TABLE IF NOT EXISTS contents (
+			author_id TEXT,
+			title_id TEXT,
+			title TEXT,
+			content TEXT,
+			PRIMARY KEY (author_id, title_id)
+		)`,
+		`CREATE VIRTUAL TABLE IF NOT EXISTS contents_fts USING fts4(words)`,
+	}
+
+	for _, query := range queries {
+		_, err := db.Exec(query)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return db, nil
+}
+
+func addEntry(db *sql.DB, entry *Entry, content string) error {
+	_, err := db.Exec(`REPLACE INTO authors(author_id, author) VALUES (?, ?)`, entry.AuthorID, entry.Author)
+	if err != nil {
+		return err
+	}
+	res, err := db.Exec(`REPLACE INTO contents(author_id, title_id, title, content) VALUES (?, ?, ?, ?)`, entry.AuthorID, entry.TitleID, entry.Title, content)
+	if err != nil {
+		return err
+	}
+
+	docID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		return err
+	}
+
+	seg := t.Wakati(content)
+	_, err = db.Exec(`REPLACE INTO contents_fts(docid, words) VALUES (?, ?)`, docID, strings.Join(seg, " "))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/nekoTheShadow/aozora-search
+
+go 1.20
+
+require golang.org/x/text v0.9.0
+
+require (
+	github.com/ikawaha/kagome-dict v1.0.9 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.10 // indirect
+	github.com/ikawaha/kagome/v2 v2.9.2 // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16
+	golang.org/x/net v0.7.0 // indirect
+)
